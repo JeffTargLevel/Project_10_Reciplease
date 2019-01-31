eedcc8bc697fd1294617b0069f313e3bcd2c27ff
JeffGitHub <jeff.santolaria05@gmail.com>
2019-01-31 17:53:50 +0100

    Create Reciplease project and import pod alamofire

---
+++ Reciplease/Reciplease/Controller/AddIngredientsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddIngredientsViewController.swift
+//  Reciplease
+//
+//  Created by Jean-François Santolaria on 31/01/2019.
+//  Copyright © 2019 OpenClassroomsFRSantolariaJF. All rights reserved.
+//
+
+import UIKit
+
+class AddIngredientsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
